8b43c0536b4a042a4934fe50d07f0fef54a5a109
bosapara <38753015+bosapara@users.noreply.github.com>
2019-07-06 18:10:55 +0400

    up

---
+++ init.lua
@@ -0,0 +1,7 @@
+minetest.register_on_chat_message(function(name, message, pos)    
+	local checkingmessage=string.lower( name.." "..message .." " )
+	if string.find(checkingmessage, "(c@", 1, true) ~=nil then
+		minetest.chat_send_player(name, "<Mr.Bot> "..name.. ", please don't abuse chat, turn off your chat csm mod.")
+        return true
+    end
+end)
